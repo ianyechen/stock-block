7985b8c39b16e067f0b948502458136fd7c47223
ianc999 <44624592+ianc999@users.noreply.github.com>
2020-05-12 21:09:47 -0700

    Changed path to database on Heroku

--- server.js
+++ server.js
@@ -98,7 +98,7 @@
 const express = require("express");
 const server = express();
 server.use(function (req, res, next) {
-    res.setHeader('Access-Control-Allow-Origin', 'http://localhost:4200');
+    res.setHeader('Access-Control-Allow-Origin', 'https://manage-my-stocks.herokuapp.com/');
     res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
     res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With, content-type');
     res.setHeader('Access-Control-Allow-Credentials', true);
